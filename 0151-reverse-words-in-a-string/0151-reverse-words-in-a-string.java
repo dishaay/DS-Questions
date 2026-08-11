ef2d9c64fc1c85ff7fa25002bcfb0bf52cdf6214
dishaa <111271318+dishaay@users.noreply.github.com>
2026-08-11 21:07:44 +0530

    Time: 8 ms (34.51%), Space: 44.7 MB (27.57%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,32 +1,33 @@
 class Solution {
     public String reverseWords(String s) {
-        StringBuilder cur = new StringBuilder();
-        Stack<String> st = new Stack<>();
-
-        for (int i = 0; i < s.length(); i++) {
-
-            if (s.charAt(i) == ' ' && cur.length() > 0) {
-                st.push(cur.toString());
-                cur.setLength(0);
-            } else if (s.charAt(i) != ' ') {
-                cur.append(s.charAt(i));
-            }
-        }
-
-        if (cur.length() > 0) {
-            st.push(cur.toString());
-        }
-
-        StringBuilder word = new StringBuilder();
-
-        while (!st.isEmpty()) {
-            if (word.length() > 0) {
-                word.append(" ");
+        StringBuilder curr= new StringBuilder();
+        StringBuilder ans= new StringBuilder();
+        Stack <String> st= new Stack <>();
+        
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==' ' && curr.length()>0){
+                //space is encountered and my string has something in it. 
+                st.push(curr.toString());
+                curr.setLength(0);
             }
 
-            word.append(st.pop());
+            else if(s.charAt(i)==' '){
+                continue;
+            }
+
+            else{
+                curr.append(s.charAt(i));
+            }
+        }
+        st.push(curr.toString());
+        while(!st.isEmpty()){
+            if(ans.length()>0){
+             ans.append(" ");
+
+            }
+            ans.append(st.pop());
         }
 
-        return word.toString();
+        return ans.toString(); 
     }
 }
