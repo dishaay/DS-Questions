4f50372e4c34b38777a1f512e2cdeb5a03a34c3f
dishaa <111271318+dishaay@users.noreply.github.com>
2026-08-18 01:12:48 +0530

    Time: 0 ms (100%), Space: 44.9 MB (12.44%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int searchInsert(int[] arr, int target) {
+        int low=0 ; 
+        int high= arr.length-1; 
+        int ans=arr.length; //edge case covered here.  
+        while(low<=high){
+            int mid = (low+high)/2;
+
+            if(arr[mid]>=target){
+                ans= mid; 
+                high=mid-1;
+            }
+
+            else {
+                low=mid+1;
+            }
+        }
+
+        return ans; 
+    }
+}
