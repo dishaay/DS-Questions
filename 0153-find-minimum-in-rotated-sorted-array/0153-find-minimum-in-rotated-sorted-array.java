ee5e856f913a22e151e880a752d14b9b69923bf1
dishaa <111271318+dishaay@users.noreply.github.com>
2026-08-22 01:13:34 +0530

    Time: 0 ms (100%), Space: 43.9 MB (20.65%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -6,15 +6,14 @@ class Solution {
         while(low<high){
             int mid=(low+high)/2;
 
-            if(arr[mid]>arr[high]){
-                low=mid+1;
+            if(arr[mid]<arr[high]){
+                high=mid;
             }
 
             else{
-                high=mid;
+                low=mid+1;
             }
         }
-
         return arr[low];
     }
 }
